5dfca5c7112ccc1ddd7ba19f30f4c75f6c5de991
jroblar <jroblar98@gmail.com>
2026-02-16 12:53:49 -0600

    update notebooks

--- ssp_modeling/output_postprocessing/postprocessing_bulgaria.r
+++ ssp_modeling/output_postprocessing/postprocessing_bulgaria.r
@@ -40,10 +40,10 @@ source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r')
 
 source('ssp_modeling/output_postprocessing/scr/data_prep_new_mapping.r')
 
-# source('ssp_modeling/output_postprocessing/scr/data_prep_drivers.r')
+source('ssp_modeling/output_postprocessing/scr/data_prep_drivers.r')
 
 # Levers table
-# source('ssp_modeling/output_postprocessing/scr/levers_table/#create levers table.r')
+source('ssp_modeling/output_postprocessing/scr/levers_table/#create levers table.r')
 
 # # Jobs table
 # source('ssp_modeling/output_postprocessing/scr/levers_table/#create jobs table.r')
